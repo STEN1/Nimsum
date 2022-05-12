9eb50f07132ef3f1221b68332330e4e908224c12
Sten Rune Lyngmo <stenrunelyngmo@gmail.com>
2022-05-12 20:39:08 +0200

    Mathias Eek edition

--- Main.cpp
+++ Main.cpp
@@ -21,4 +21,34 @@ int main()
 		nimsum ^= stack;
 
 	std::cout << "Nimsum: " << nimsum << std::endl;
+
+    if (nimsum == 0)
+        return 0;
+
+    std::vector<int> subtractStack(numberOfStacks);
+
+    for (int i = 0; i < numberOfStacks; ++i)
+    {
+        std::vector<int> tempStack = stacks;
+
+        for (int j = 0; j < stacks[i]; ++j)
+        {
+            tempStack[i]--;
+            int newNimsum{};
+            for (int stack : tempStack)
+                newNimsum ^= stack;
+
+            if (newNimsum == 0)
+            {
+                subtractStack[i] = stacks[i] - tempStack[i];
+                break;
+            }
+        }
+    }
+
+    std::cout << std::endl;
+    std::cout << "Options for zeroing the nimsum" << std::endl;
+
+    for (int i = 0; i < subtractStack.size(); i++)
+        std::cout << "Subtract " << subtractStack[i] << " from stack " << i << std::endl;
 }
